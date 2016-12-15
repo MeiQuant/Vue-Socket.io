47590d649bfccbf92a0bd83a193ce338893c8692
onthedesk <xiuguozhao@gmail.com>
2016-12-15 11:14:52 +0800

    upgrade tools to webpack v2

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,48 @@
+/**
+ * Created by alfred on 2016/12/14.
+ */
 var path = require('path');
 var webpack = require('webpack');
 module.exports = {
-    entry: ['./src/Main.js'],
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'build.js',
-        library: ['VueSocketio'],
-        libraryTarget: 'umd'
-    },
-    resolveLoader: {
-        root: path.join(__dirname, 'node_modules'),
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel',
-                exclude: /node_modules/,
-				query: {
-				  presets: ['es2015']
-				}
-            },
-            {
-                test: /\.json$/,
-                loader: 'json'
-            }
-        ]
-    },
-    devtool: 'eval-source-map'
+  entry: ['./src/Main.js'],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'build.js',
+    library: ['VueSocketioMeiQuant'],
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      }
+    ]
+  },
+  devtool: '#eval-source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports.devtool = 'source-map'
+  module.exports.devtool = '#source-map'
 
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-        new webpack.optimize.OccurenceOrderPlugin()
-    ])
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+  ])
 }
-
